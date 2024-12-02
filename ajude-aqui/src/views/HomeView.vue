5a37ac2248e73bc87ceb8e24c9e766b78794315e
francivaldo4334 <francivaldodev@gmail.com>
2024-12-02 10:01:17 -0300

    :refactor: Limpa o projeto para ficar mais simples

--- ajude-aqui/src/views/HomeView.vue
+++ ajude-aqui/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <HelloWorld/>
   </main>
 </template>
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
+
