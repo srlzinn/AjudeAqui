47af07ad215bb0dcde7fabff615f962224e60a2c
francivaldo4334 <francivaldodev@gmail.com>
2024-12-03 11:06:08 -0300

    :sparkles: feat: adiciona campo de categoria em serviços e adiciona modleo de habilidade e repositorio de habilidade

---
+++ ajude-aqui/src/composables/data/models/Habilidade.js
@@ -0,0 +1,9 @@
+export default class {
+  id
+  habilidade
+  usuario
+  constructor(data) {
+    this.habilidade = data.habilidade;
+    this.usuario = data.usuario;
+  }
+}
--- ajude-aqui/src/composables/data/models/Serviço.js
+++ ajude-aqui/src/composables/data/models/Serviço.js
@@ -3,9 +3,11 @@ export default class {
   titulo
   contratante
   disponivel
+  categoria
   constructor(data) {
     this.titulo = data.titulo;
     this.contratante = data.contratante;
     this.disponivel = data.disponivel;
+    this.categoria = data.categoria;
   }
 }
