0edd6462357f89c881fb0df39788d483e0eb9c94
francivaldo4334 <francivaldodev@gmail.com>
2024-12-03 11:37:13 -0300

    :sparkles: feat: adiciona listagem e inserção de contratações

--- ajude-aqui/src/views/Contratacoes.vue
+++ ajude-aqui/src/views/Contratacoes.vue
@@ -1,35 +1,18 @@
 <template>
   <div>
-    <v-list :items="contratacoes.map(c=>c.servicos)" class="mb-4"></v-list>
+    <v-list :items="contratacoes.map(c => `${c.servico} - ${c.contratado}`)" class="mb-4"></v-list>
     <div class="d-flex w-100 justify-end">
-      <v-btn
-        size="64"
-        icon="mdi-plus"
-        color="primary"
-        @click="modalContratar = true"
-      ></v-btn>
+      <v-btn size="64" icon="mdi-plus" color="primary" @click="modalContratar = true"></v-btn>
     </div>
     <v-dialog v-model="modalContratar" width="auto">
       <v-card min-width="400" class="pa-4">
         <v-form ref="formContratacao">
-          <v-select 
-            label="Usuário" 
-            :items="usuarios" 
-            v-model="novaContratacao.usuario" 
-            item-text="nome" 
-            item-value="id" 
-            variant="outlined" 
-            :rules="[v => !!v || 'Obrigatório']"
-          ></v-select>
-          <v-select 
-            label="Serviço" 
-            :items="servicos" 
-            v-model="novaContratacao.servico" 
-            item-text="nome" 
-            item-value="id" 
-            variant="outlined" 
-            :rules="[v => !!v || 'Obrigatório']"
-          ></v-select>
+          <v-text-field type="date" v-model="novaContratacao.data_de_contratacao">
+          </v-text-field>
+          <v-text-field v-model="novaContratacao.servico">
+          </v-text-field>
+          <v-text-field v-model="novaContratacao.contratado">
+          </v-text-field>
           <div class="d-flex gap-4 justify-end">
             <v-btn @click="fecharModalContratar" color="error" variant="plain">Cancelar</v-btn>
             <v-btn @click="criarContratacao" color="green">Salvar</v-btn>
@@ -42,10 +25,14 @@
 
 <script setup lang="ts">
 import { ref, reactive, onMounted, inject } from "vue";
-import {VList, VBtn, VDialog, VCard, VForm, VTextField} from "vuetify/components"
+import { VList, VBtn, VDialog, VCard, VForm, VTextField } from "vuetify/components"
 const banco = inject("$banco");
 const contratacoes = reactive([]);
-const novaContratacao = reactive({ usuario: null, servico: null });
+const novaContratacao = reactive({
+  data_de_contratacao: "",
+  servico: "",
+  contratado: "",
+});
 const modalContratar = ref(false);
 const formContratacao = ref(null);
 
@@ -57,10 +44,16 @@ function fecharModalContratar() {
 function criarContratacao() {
   formContratacao.value.validate().then((res) => {
     if (res) {
-      banco.repositorioDeContratos.criar(novaContratacao, (contratacao) => {
-        contratacoes.push(contratacao);
-        fecharModalContratar();
-      });
+      banco.repositorioDeContratos.criar(
+        {
+          data_de_contratacao: novaContratacao.data_de_contratacao,
+          servico: novaContratacao.servico,
+          contratado: novaContratacao.contratado,
+        },
+        (contratacao) => {
+          contratacoes.push(contratacao);
+          fecharModalContratar();
+        });
     }
   });
 }
